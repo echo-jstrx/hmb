4ecd9abe7ce4f22385d6fd9322a1fe091f551b2e
jstrx <162533962+echo-jstrx@users.noreply.github.com>
2024-05-31 11:13:57 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,24 @@
-const menu = document.querySelector(".menu");
-const menuItems = document.querySelectorAll(".menuItem");
-const hamburger = document.querySelector(".hamburger");
-const closeIcon = document.querySelector(".closeIcon");
-const menuIcon = document.querySelector(".menuIcon");
-
-function toggleMenu() {
-    if (menu.classList.contains("showMenu")) {
-        menu.classList.remove("showMenu");
-        closeIcon.style.display = "none";
-        menuIcon.style.display = "block";
-    } else {
-        menu.classList.add("showMenu");
-        closeIcon.style.display = "block";
-        menuIcon.style.display = "none";
+window.addEventListener("DOMContentLoaded", (event) => {
+    const menu = document.querySelector(".menu");
+    const menuItems = document.querySelectorAll(".menuItem");
+    const hamburger = document.querySelector(".hamburger");
+    const closeIcon = document.querySelector(".closeIcon");
+    const menuIcon = document.querySelector(".menuIcon");
+    
+    function toggleMenu() {
+        if (menu.classList.contains("showMenu")) {
+            menu.classList.remove("showMenu");
+            closeIcon.style.display = "none";
+            menuIcon.style.display = "block";
+        } else {
+            menu.classList.add("showMenu");
+            closeIcon.style.display = "block";
+            menuIcon.style.display = "none";
+        }
     }
-}
-if(hamburger){
-    hamburger.addEventListener("click", toggleMenu);
-} else {
-    alert ("Error! Menu not found");
+    if(hamburger){
+        hamburger.addEventListener("click", toggleMenu);
+    } else {
+        alert ("Error! Menu not found");
+    }
 }
