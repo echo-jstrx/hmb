65086e88c0ad951ef25b980b6f337b328464c50a
jstrx <162533962+echo-jstrx@users.noreply.github.com>
2024-06-03 09:10:23 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 window.addEventListener("DOMContentLoaded", (event) => {
     const menu = document.querySelector(".menu");
-    const menuItems = document.querySelectorAll(".menuItem");
+    // const menuItems = document.querySelectorAll(".menuItem");
     const hamburger = document.querySelector(".hamburger");
     const closeIcon = document.querySelector(".closeIcon");
     const menuIcon = document.querySelector(".menuIcon");
